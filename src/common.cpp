131a5193150fcfa9d1e987d8fd68114247d7206b
sooyka <bartosz.sjka@gmail.com>
2023-01-16 10:36:57 +0100

    fixed gitignore

--- src/common.cpp
+++ src/common.cpp
@@ -1,8 +1,8 @@
 #include "common.h"
 
-Matrix4f exponential_coordinates_to_se3(const coordinates&){
-    Matrix4f lie_alg_matrix = [1,0,0,0,
-                                0,1,0,0,
-                                0.0f,0.0f,1.0f,0.0f,
-                                0.0f,0.0f,0.0f,1.0f]; 
-}
+// Matrix4f exponential_coordinates_to_se3(const coordinates&){
+//     Matrix4f lie_alg_matrix = [1,0,0,0,
+//                                 0,1,0,0,
+//                                 0.0f,0.0f,1.0f,0.0f,
+//                                 0.0f,0.0f,0.0f,1.0f]; 
+// }
